6d728563566683b0ce61360aa7ab1e03cb0a94af
Erik de Kamps <erikdekamps@gmail.com>
2017-10-17 17:52:15 +0200

    Initial commit for the theme.

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Custom javascript for cheatsheet_theme.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  /**
+   * Custom javascript.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.custom = {
+    attach: function () {
+      
+    }
+  };
+})(jQuery, Drupal);
